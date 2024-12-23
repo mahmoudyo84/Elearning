404bcaf082a14d5b70e335f34d83033b0a03c208
mohamed-ashraf <mohamef.ashraf877@gmail.com>
2024-12-23 15:55:57 +0200

    quiz finshed and i changed buttons color for all project

--- project/static/js/quiz.js
+++ project/static/js/quiz.js
@@ -1,14 +1,14 @@
 window.onscroll = function () {
-    myScrollFunction();
-  };
-  
-  function myScrollFunction() {
-    if (
-      document.body.scrollTop > 100 ||
-      document.documentElement.scrollTop > 100
-    ) {
-      document.getElementById("header").classList.add("animateHeader");
-    } else {
-      document.getElementById("header").classList.remove("animateHeader");
-    }
+  myScrollFunction();
+};
+
+function myScrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    document.getElementById("header").classList.add("animateHeader");
+  } else {
+    document.getElementById("header").classList.remove("animateHeader");
   }
+}
