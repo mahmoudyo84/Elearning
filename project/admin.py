547c06e40489fec155ef1285da9a9f109660641c
ahmedmaher09 <ahmedmaher0870@gmail.com>
2024-12-26 14:48:31 +0200

    Update admin.py

--- project/admin.py
+++ project/admin.py
@@ -1,4 +1,4 @@
-'''
+
 from django.contrib import admin
 
 # Register your models here.
@@ -21,79 +21,4 @@ admin.site.register(CourseDetails)
 admin.site.register(CourseMedia)
 admin.site.register(UserCourses)
 from django.contrib import admin
-from .models import Instructor, Course, Enrollment
-
-'''
-
-from django.contrib import admin 
-from .models import (
-    Roles, Screens, Privileges, Users, Categories,
-    Courses, CourseDetails, CourseMedia, UserCourses,
-    CoursesExams, ExamsQuestions, QuestionsChoices,
-    ExamAnswer, UsersExams, Instructor, Course, Enrollment,
-    Exam, Question, Answer, UserProfile
-)
-
-
-#  Roles (الأدوار)
-admin.site.register(Roles)  
-
-#  Screens (الشاشات)
-admin.site.register(Screens)  
-
-#  Privileges (الصلاحيات)
-admin.site.register(Privileges)  
-
-#  Users (المستخدمين)
-admin.site.register(Users)  
-
-#  Categories (الفئات)
-admin.site.register(Categories)  
-
-#  Courses (الدورات)
-admin.site.register(Courses)  
-
-#  CourseDetails (تفاصيل الدورة)
-admin.site.register(CourseDetails)  
-
-#  CourseMedia (الوسائط المرتبطة بالدورة)
-admin.site.register(CourseMedia)  
-
-#  UserCourses (الدورات التي سجل فيها المستخدم)
-admin.site.register(UserCourses)  
-
-#  CoursesExams (امتحانات الدورات)
-admin.site.register(CoursesExams)  
-
-#  ExamsQuestions (أسئلة الامتحانات)
-admin.site.register(ExamsQuestions)  
-
-#  QuestionsChoices (خيارات الإجابة على الأسئلة)
-admin.site.register(QuestionsChoices)  
-
-#  ExamAnswer (إجابات الامتحانات)
-admin.site.register(ExamAnswer)  
-
-#  UsersExams (نتائج الامتحانات الخاصة بالمستخدمين)
-admin.site.register(UsersExams)  
-
-#  Instructor (المدرب)
-admin.site.register(Instructor)  
-
-#  Course (الدورة)
-admin.site.register(Course)  
-
-#  Enrollment (التسجيلات في الدورات)
-admin.site.register(Enrollment)  
-
-#  Exam (الامتحان)
-admin.site.register(Exam)  
-
-#  Question (السؤال)
-admin.site.register(Question)  
-
-#  Answer (الإجابة)
-admin.site.register(Answer)  
-
-#  UserProfile (الملف الشخصي للمستخدم)
-admin.site.register(UserProfile)  
+from .models import Instructor, Course, Enrollm 
