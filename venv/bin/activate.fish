3abdfa8eed3b21de47e6d0ba6f837f1908702ffb
mahmoudyo <mahmoud.youssef.tak@gmail.com>
2024-12-27 11:16:34 +0200

    finished Fixing some fixes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/mahmouyo/Desktop/Elearning/venv
+set -gx VIRTUAL_ENV /home/mahmoud/Desktop/Elearning/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
